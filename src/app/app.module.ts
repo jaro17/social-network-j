9ac4ec3a6a558c7f0293ce54617453e3a14ccec2
jaro17 <jaroslav.banas@gmail.com>
2022-06-09 16:28:57 +0200

    New project.master

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from "@angular/common/http";
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+
+// @ts-ignore
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    TopbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
